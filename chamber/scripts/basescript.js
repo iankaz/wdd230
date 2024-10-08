b0d1ce56afd43230752aec46c688b3930cc8bde7
iankaz <iankazembe41@gmail.com>
2024-10-08 06:13:09 +0200

    ...

--- chamber/scripts/basescript.js
+++ chamber/scripts/basescript.js
@@ -39,3 +39,57 @@ window.addEventListener('scroll', () => {
         loadImages();
     }
 });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const visitMessageElement = document.getElementById('visit-message');
+        const lastVisit = localStorage.getItem('lastVisit');
+        const now = Date.now();
+        
+        if (!lastVisit) {
+            // First visit
+            visitMessageElement.textContent = "Welcome! Let us know if you have any questions.";
+        } else {
+            const lastVisitDate = new Date(parseInt(lastVisit));
+            const timeDifference = now - lastVisitDate.getTime();
+            const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            
+            if (timeDifference < 24 * 60 * 60 * 1000) {
+                // Less than a day
+                visitMessageElement.textContent = "Back so soon! Awesome!";
+            } else if (daysDifference === 1) {
+                // Exactly one day
+                visitMessageElement.textContent = "You last visited 1 day ago.";
+            } else {
+                // More than one day
+                visitMessageElement.textContent = `You last visited ${daysDifference} days ago.`;
+            }
+        }
+        
+        // Update the last visit date in localStorage
+        localStorage.setItem('lastVisit', now.toString());
+
+        // Date math logic
+        const msToDays = 86400000;
+        const theDateToday = new Date();
+
+        // Initialize display elements
+        const todayElement = document.querySelector("#today");
+        const christmasElement = document.querySelector("#christmas");
+        const christmasDateElement = document.querySelector("#christmasDate");
+        const daysElement = document.querySelector("#daysleft");
+
+        // Processing
+        const today = Date.now();
+        const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
+        // Check if it is the waning days of December, if so ... change to next year.
+        if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
+            christmasDate.setFullYear(christmasDate.getFullYear() + 1);
+        }
+        // Find difference between epoch times in ms and convert to days
+        let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
+
+        todayElement.textContent = today;
+        christmasElement.textContent = christmasDate.getTime();
+        christmasDateElement.textContent = christmasDate;
+        daysElement.textContent = `${daysleft.toFixed(0)} days`;
+    });
