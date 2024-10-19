77f5fdc2ca8ef01be2006b06a9495b16939f8e5f
iankaz <iankazembe41@gmail.com>
2024-10-19 02:24:17 +0200

    updated directory page

---
+++ chamber/scripts/members.js
@@ -0,0 +1,38 @@
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector("#member-display");
+
+async function getMembers() {
+    const response = await fetch('data/members.json');
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+    display.innerHTML = '';
+    members.forEach(member => {
+        const memberElement = document.createElement('section');
+        memberElement.innerHTML = `
+            <img src="images/${member.image}" alt="${member.name}">
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <p><a href="${member.website}" target="_blank">${member.website}</a></p>
+            <p>${member.membershipLevel}</p>
+            <p>${member.description}</p>
+        `;
+        display.appendChild(memberElement);
+    });
+}
+
+gridButton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+});
+
+getMembers();
